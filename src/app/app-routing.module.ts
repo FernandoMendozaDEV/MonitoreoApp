1ca1991ca0a2904769fa84320ea36a0f78eec4f6
Fernando Mendoza <ferelmendolop@gmail.com>
2021-04-05 14:02:23 -0600

    Maquetacion del sistema 1/2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,48 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContainerAppComponent } from './components/pages/container-app/container-app.component';
+import { DashboardComponent } from './components/pages/dashboard/dashboard.component';
+import { InstalacionesComponent } from './components/pages/instalaciones/instalaciones.component';
+import { VisitasComponent } from './components/pages/visitas/visitas.component';
+import { RetirosComponent } from './components/pages/retiros/retiros.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full'
+  },
+  {
+    path: '',
+    component: ContainerAppComponent,
+    children: [
+      {
+        path: 'dashboard',
+        component: DashboardComponent,
+      },
+      {
+        path: 'instalaciones',
+        component: InstalacionesComponent,
+      },
+      {
+        path: 'visitas',
+        component: VisitasComponent,
+      },
+      {
+        path: 'retiros',
+        component: RetirosComponent,
+      }
+    ]
+  },
+  {
+    path:'**',
+    redirectTo: 'dashboard'
+  }
+
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,25 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+//COMPONENTES CREADOS
+import { SidenavComponent } from './shared/sidenav/sidenav.component';
+import { ToolbarComponent } from './shared/toolbar/toolbar.component';
+import { ContainerAppComponent } from './components/pages/container-app/container-app.component';
+import { DashboardComponent } from './components/pages/dashboard/dashboard.component';
+import { InstalacionesComponent } from './components/pages/instalaciones/instalaciones.component';
+import { VisitasComponent } from './components/pages/visitas/visitas.component';
+import { RetirosComponent } from './components/pages/retiros/retiros.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidenavComponent,
+    ToolbarComponent,
+    ContainerAppComponent,
+    DashboardComponent,
+    InstalacionesComponent,
+    VisitasComponent,
+    RetirosComponent
   ],
   imports: [
     BrowserModule,
